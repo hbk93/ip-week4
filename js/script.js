f4dff082ca9cca935bb2fb3fc1e33745c7fb1261
Harry Bett <harrybett89@gmail.com>
2021-01-12 10:06:42 +0300

    add reset function

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
     else {
       console.log("No price");
     }
-    
+
     let topping_value = ptopping.length * 50;
     console.log("toppins value" + topping_value);
 
@@ -140,8 +140,58 @@ $(document).ready(function () {
 
       $("#ordersmade").append('<tr><td id="pizzaname">' + newOrder.name + '</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">' + newOrder.crust + '</td><td id="pizzatopping">' + newOrder.topping + '</td><td id="totals">' + newOrder.total + '</td></tr>');
       console.log(newOrder);
-
-
-
     });
     
+
+    $("button#checkout").click(function () {
+      $("button#checkout").hide();
+      $("button.addPizza").hide();
+      $("button.deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      console.log("Your total bills is sh. " + checkoutTotal);
+      $("#pizzatotal").append("Your bill is sh. " + checkoutTotal);
+    });
+
+    $("button#final-order").click(function (event) {
+      event.preventDefault();
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliveryAmount = checkoutTotal + 150;
+      console.log("Final Bill is: " + deliveryAmount);
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val() != "") {
+
+        $("#finallmessage").append(person + ", We have recieved your order and it will be delivered to you at " + location + ". Prepare Ksh. " + deliveryAmount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+    
+    $("button.deliver").click(function () {
+      $(".pizzatable").hide();
+      $(".choise h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button.deliver").hide();
+      $("#pizzatotal").hide();
+      let deliveryAmount = checkoutTotal + 150;
+      console.log("You will pay sh. " + deliveryAmount + " on delivery");
+      $("#totalbill").append("Your bill plus delivery fee is: " + deliveryAmount);
+    });
+
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function () {
+  $('button.clear').trigger("reset")
+});
