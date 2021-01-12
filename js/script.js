b61d3a4598cd5cf32fa0d91a26e063dc97c821d8
Harry Bett <harrybett89@gmail.com>
2021-01-12 07:56:54 +0300

    create variables and constructor

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let price , crust_price, topping_price ;
+let total = 0;
+class Getpizza {
+  constructor(name, size, crust, topping, total) {
+    this.name = name;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.total = total;
+  }
+}
